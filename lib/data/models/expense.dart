d974145bf9b5f1435a59c2b00a71e2bd9d687d79
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-11 20:45:56 +0100

    feat(expenses): update expense domain model

--- lib/data/models/expense.dart
+++ lib/data/models/expense.dart
@@ -2,22 +2,37 @@ class ExpenseModel {
   const ExpenseModel({
     required this.id,
     required this.userId,
-    required this.date,
-    required this.amount,
+    required this.supplierName,
+    required this.description,
     required this.category,
+    required this.amountHt,
+    required this.taxRate,
+    required this.date,
+    this.notes,
   });
 
   final String id;
   final String userId;
-  final DateTime date;
-  final double amount;
+  final String supplierName;
+  final String description;
   final String category;
+  final double amountHt;
+  final double taxRate;
+  final DateTime date;
+  final String? notes;
+
+  double get taxAmount => amountHt * taxRate / 100;
+  double get totalTtc => amountHt + taxAmount;
 
   factory ExpenseModel.fromMap(Map<String, dynamic> map) => ExpenseModel(
         id: map['id'] as String,
         userId: map['user_id'] as String,
-        date: DateTime.parse(map['date'] as String),
-        amount: (map['amount'] as num).toDouble(),
+        supplierName: map['supplier_name'] as String,
+        description: map['description'] as String,
         category: map['category'] as String,
+        amountHt: (map['amount_ht'] as num).toDouble(),
+        taxRate: (map['tax_rate'] as num?)?.toDouble() ?? 0,
+        date: DateTime.parse(map['expense_date'] as String),
+        notes: map['notes'] as String?,
       );
 }
